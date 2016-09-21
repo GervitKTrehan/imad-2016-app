16dbefdfb047f958450ae178453f70b7862e7d8b
GervitKTrehan <GervitKTrehan@users.noreply.github.com>
2016-09-21 21:24:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var article = { 
+var articles = { 
     'article-one': {
     title: 'Article One | Gervit K Trehan',
     heading: 'Article One',
