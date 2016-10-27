bd15c409e6e249e8b984d707d215ef53583e1d69
GervitKTrehan <GervitKTrehan@users.noreply.github.com>
2016-10-27 23:55:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/articles/:articleName', function (req, res) {
               res.status(404).send('Article not found');
           } else {
               var articleDta = result.rows[0];
-              res.send(createTemplate(articleDta));
+              res.send(createTemplate(articleData));
           }
       }
    });
