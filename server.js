95c3684a132e881cee745d626fb0e8cdcbfd6201
GervitKTrehan <GervitKTrehan@users.noreply.github.com>
2016-10-28 00:00:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,14 +116,14 @@ app.get('/articles/:articleName', function (req, res) {
     // articleName == article-one
     // articles[articleName] == {} content object for article one
 
-   pool.query("SELECT * FROM article WHERE title = " + req.params.articleNmae, function (err, result) {
+   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleNmae + "'", function (err, result) {
       if (err) {
         res.status(500).send(err.toString());  
       } else {
           if (result.rows.length === 0) {
               res.status(404).send('Article not found');
           } else {
-              var articleDta = result.rows[0];
+              var articleData = result.rows[0];
               res.send(createTemplate(articleData));
           }
       }      
